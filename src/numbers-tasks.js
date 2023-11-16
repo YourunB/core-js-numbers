67e7e8edafa582408b6b85a1d01b4719b19ba6c1
YourunB <bxyura@gmail.com>
2023-11-16 17:37:49 +0300

    feat: complete isInteger func

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -443,8 +443,10 @@ function isNumber(number) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  let res = false;
+  if (Number.isInteger(number)) res = true;
+  return res;
 }
 
 /**
