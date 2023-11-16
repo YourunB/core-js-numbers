a11b3a54574af836bcf81a934366d1601a9fa2a2
YourunB <bxyura@gmail.com>
2023-11-16 14:32:06 +0300

    feat: complete getFibonacciNumber func

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -255,8 +255,10 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const arr = [0, 1];
+  for (let i = 2; i <= index; i += 1) arr.push(arr[i - 1] + arr[i - 2]);
+  return arr[index];
 }
 
 /**
